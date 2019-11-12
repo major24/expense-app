b533df993150cf833c0c1590ae9a9499a36c96d9
major24 <major.nalliah@gmail.com>
2019-11-12 13:35:32 -0500

    Updated transaction class with default

--- client-vue/src/models/transaction.js
+++ client-vue/src/models/transaction.js
@@ -1,14 +1,14 @@
-function Transaction () {
-  id = '',
-  transType = '',
-  description = '',
-  amount = 0,
-  tax = 0,
-  transDate = '',
-  category = '',
-  isPersonal = false,
-  isRemove = false,
-  isOutOfPocket = false
+export default class Transaction {
+  constructor (id, transType, description, amount, tax, transDate, category, isPersonal = false, isRemove = false, isOutOfPocket = false) {
+    this.id = id
+    this.transDate = transType
+    this.description = description
+    this.amount = amount
+    this.tax = tax
+    this.transDate = transDate
+    this.category = category
+    this.isPersonal = isPersonal
+    this.isRemove = isRemove
+    this.isOutOfPocket = isOutOfPocket
+  }
 }
-
-module.exports = Transaction
